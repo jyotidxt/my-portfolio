9c28e290a6a7d4f9454006708a239d0be0524c2e
jyotidxt <dixit.jyoti004@gmail.com>
2024-12-06 21:14:27 +0530

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,11 @@
+// let button = document.querySelector("#button");
+// button.addEventListener('click', function() {
+//     button.sstyle.backgroundColor = "skyblue";
+// })
+
+
+// $(document).ready(function(){
+//     $('#icon').click(function(){
+//         $('ul').toggleClass('show');
+//     })
+//     });
